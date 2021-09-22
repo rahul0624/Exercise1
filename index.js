c7a32d72c02001bbbb07728e8b6973f9162e2ba2
Rahul Suddala <rahul.suddala@gmail.com>
2021-09-22 17:46:43 +0530

    . .

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
+document.getElementsByClassName("click-button")[0].onclick =
+  function increment() {
+    document.getElementsByClassName("c-count")[0].innerHTML =
+      parseInt(document.getElementsByClassName("c-count")[0].innerHTML) + 1;
+  };
 
-
-document.getElementsByClassName("click-button")[0].onclick = function increment(){
-    document.getElementsByClassName("c-count")[0].innerHTML = 
-        parseInt(document.getElementsByClassName("c-count")[0].innerHTML)+1;
+document.getElementsByClassName("reset-button")[0].onclick = function reset() {
+  document.getElementsByClassName("c-count")[0].innerHTML = 0;
 };
-
-document.getElementsByClassName("reset-button")[0].onclick = function reset(){
-    document.getElementsByClassName("c-count")[0].innerHTML = 0;
-
-}
-
